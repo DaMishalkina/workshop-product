1901666da5f97a66985880d85145395682d48d2b
alisakova <a.lisakova@3commas.io>
2021-05-07 10:56:07 +0300

    fix paths

--- design-system/src/tokens/index.js
+++ design-system/src/tokens/index.js
@@ -3,9 +3,9 @@
  * If you are adding new token category, import it here
  */
 
-import * as colors from "../tokens/dist/colors";
-import * as typography from "../tokens/dist/typography";
-import * as spacing from "../tokens/dist/spacing";
-import * as borderRadius from "../tokens/dist/borderRadius";
+import * as colors from "design-system/src/tokens/dist/colors";
+import * as typography from "design-system/src/tokens/dist/typography";
+import * as spacing from "design-system/src/tokens/dist/spacing";
+import * as borderRadius from "design-system/src/tokens/dist/borderRadius";
 
 export { colors, typography, spacing, borderRadius };
